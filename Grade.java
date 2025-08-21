b850afcaf43de626c96d627924ab0e80f6cd172f
Saeid <sayeedbinomar27@gmail.com>
2025-08-21 19:13:25 +0600

    Added Grade.java with grading methods

---
+++ Grade.java
@@ -0,0 +1,10 @@
+public class Grade {
+    public String getLetterGrade(int marks) {
+        if (marks >= 90) return "A";
+        else if (marks >= 75) return "B";
+        else if (marks >= 60) return "C";
+        else if (marks >= 40) return "D";
+        else return "F";
+    }
+}
+
