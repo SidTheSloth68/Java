3c24933f0ab91cb78daa7947febec001e51fc443
Saeid <sayeedbinomar27@gmail.com>
2025-08-21 18:53:44 +0600

    master: add/change Calculator.java (will conflict)

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,6 @@
+// Version on MAIN (intentionally different)
 public class Calculator {
-    public int add(int a, int b) { return a + b; }
+    public int add(int a, int b) { return a + b; }          // <-- will conflict
     public int subtract(int a, int b) { return a - b; }
     public int multiply(int a, int b) { return a * b; }
     public double divide(int a, int b) { return (double) a / b; }
